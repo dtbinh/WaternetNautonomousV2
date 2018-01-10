0dd6da51cbc3d6d9993bbb24caa270f27bcc75dd
daleyadrichem <daleyadrichem@upcmail.nl>
2018-01-10 16:30:40 +0100

    Fixes

--- src/nautonomous_planning_and_control_using_search/bashscripts/run_cornered_map_with_sim_obstacle.sh
+++ src/nautonomous_planning_and_control_using_search/bashscripts/run_cornered_map_with_sim_obstacle.sh
@@ -34,6 +34,13 @@ rostopic pub --once /mission_coordinator/obstacle nautonomous_mpc_msgs/Obstacle 
 major_semiaxis: $(( ( RANDOM % 6 )  + 3 ))
 minor_semiaxis: $(( ( RANDOM % 3 )  + 3 ))" &
 
+#sleep 2
+#rostopic pub --once /mission_coordinator/start nautonomous_mpc_msgs/StageVariable "{T_l: 0.0, T_r: 0.0, x: -112.0, y: 18.0, theta: 0.0, u: 0.0, v: 0.0, omega: 0.0}" &
+
+#sleep 10
+
+#rostopic pub --once /mission_coordinator/goal nautonomous_mpc_msgs/StageVariable "{T_l: 0.0, T_r: 0.0, x: 115.0, y: 15.0, theta: 0.0, u: 0.0, v: 0.0, omega: 0.0}" &
+
 sleep 1
 
 rostopic pub --once /mission_coordinator/start nautonomous_mpc_msgs/StageVariable "{T_l: 0.0, T_r: 0.0, x: -112.0, y: 18.0, theta: 0.0, u: 0.0, v: 0.0, omega: 0.0}" &
