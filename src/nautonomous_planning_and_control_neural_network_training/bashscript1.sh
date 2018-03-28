d8ea94a93fad6f94bb5728f0033fbd4f8801bc05
Waternet <rob.van.putten@waternet.nl>
2018-03-28 13:40:39 +0200

    old bash script

--- src/nautonomous_planning_and_control_neural_network_training/bashscript1.sh
+++ src/nautonomous_planning_and_control_neural_network_training/bashscript1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rosservice call /load "config_name: '/home/daley/WaternetNautonomousV2/src/nautonomous_planning_and_control_neural_network_training/include/amsterdam_cropped.yaml'"  &
+rosservice call /load "config_name: '/home/waternet/programmeren/nautonomous/WaternetNautonomousV2/src/nautonomous_planning_and_control_neural_network_training/include/amsterdam_cropped.yaml'"  &
 
 sleep 1 &
 
