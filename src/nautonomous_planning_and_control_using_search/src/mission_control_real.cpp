b70374041e44dc42d5ad297ce075ec480e007f66
daleyadrichem <daleyadrichem@upcmail.nl>
2018-08-07 13:31:38 +0200

    Mission control

--- src/nautonomous_planning_and_control_using_search/src/mission_control_real.cpp
+++ src/nautonomous_planning_and_control_using_search/src/mission_control_real.cpp
@@ -215,6 +215,7 @@ int main(int argc, char **argv)
 		listener.lookupTransform("/lidar_link", "/occupancy_grid", ros::Time(0), transform_lidar_grid);
 
 		Current_loop_time = ros::Time::now().toSec();
+                ROS_DEBUG_STREAM("Time checked");
 		if(borders_received && current_state_received)
 		{
 			if (Current_loop_time - Time_of_last_path_call > 2)
